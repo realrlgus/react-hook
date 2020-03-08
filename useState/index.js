4923c0816366051a17e3890d35171bb158aa27d6
realrlgus <realrlgus@naver.com>
2020-03-09 00:05:12 +0900

    Add useState , useInput

--- useState/index.js
+++ useState/index.js
@@ -1,1 +1,19 @@
-export { useInput as default } from "./useInput";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
