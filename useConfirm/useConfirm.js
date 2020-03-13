c89366da5337080b38533f1996c02008d037a4dc
realrlgus <realrlgus@naver.com>
2020-03-13 23:25:48 +0900

    add useConfirm, usePrevent

---
+++ useConfirm/useConfirm.js
@@ -0,0 +1,16 @@
+export default useConfirm = (message = "", callback, rejection) => {
+  if (!callback || typeof callback !== "function") {
+    return;
+  }
+  if (!rejection || typeof rejection !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (confirm(message)) {
+      callback();
+    } else {
+      rejection();
+    }
+  };
+  return confirmAction;
+};
